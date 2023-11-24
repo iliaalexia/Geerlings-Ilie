86a587ca54a2d284e8db77d9ee9195206a296f0e
kgeerlings <kgeerlings@ensc.fr>
2023-11-24 10:21:36 +0100

    version 1

--- programme/projetXmas/Program.cs
+++ programme/projetXmas/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char[,] Matrice(int taille)
+{
+    char[,] matriceJeu= new char[taille, taille];
+    for (int i=0; i<taille; i++)
+    {
+        for (int j=0; j<taille; j++)
+        {
+            matriceJeu[i,j]= ' ';
+        }
+    }
+    return matriceJeu;
+}
+
+Console.WriteLine(Matrice(4));
