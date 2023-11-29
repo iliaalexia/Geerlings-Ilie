2dea3e302807d622125543c9fccb0e90c1734b88
kgeerlings <kgeerlings@ensc.fr>
2023-11-29 01:05:52 +0100

    version 5, problème sur le deplacement a droite et en bas à regler et score à rajouter

--- programme/projetXmas/Program.cs
+++ programme/projetXmas/Program.cs
@@ -16,7 +16,19 @@ char[,] Matrice(int taille)
     return matriceJeu;
 }
 
-//char[,] tab = Matrice(4);
+//programme créant une matrice rempli de 0
+int[,] MatriceEntiers(int taille)
+{
+    int[,] matriceJeu = new int[taille, taille];
+    for (int i = 0; i < taille; i++)
+    {
+        for (int j = 0; j < taille; j++)
+        {
+            matriceJeu[i, j] = 0;
+        }
+    }
+    return matriceJeu;
+}
 
 
 
@@ -29,12 +41,38 @@ char[,] SymboleMatrice(char[,] matrice)
     matrice[ligne, colonne] = '*';
     return matrice;
 }
-//char[,] tab1 = SymboleMatrice(Matrice(4));
 
+
+//programme placant un 1 dans la matrice sur une position aléatoire
+int[,] SymboleMatrice2(int[,] matrice)
+{
+    Random aleatoire = new Random();
+    int ligne = aleatoire.Next(0, matrice.GetLength(0));
+    int colonne = aleatoire.Next(0, matrice.GetLength(1));
+    matrice[ligne, colonne] = 2;
+    return matrice;
+}
 
 
 //programme affichant la matrice avec un joli design
 void AfficherMatrice(char[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            Console.Write($"| {tab[i, j]} ");
+            if (j == tab.GetLength(1) - 1)
+            {
+                Console.Write("|");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+//programme affichant une matrice d'entiers
+void AfficherMatriceEntiers(int[,] tab)
 {
     for (int i = 0; i < tab.GetLength(0); i++)
     {
@@ -55,6 +93,7 @@ void AfficherMatrice(char[,] tab)
 //programme du jeu
 Console.WriteLine("Est-ce que vous connaissez les règles du jeu? Repondez par oui ou non");
 string reponse = Console.ReadLine()!;
+
 if (reponse == "non" || reponse == "Non")
     Console.WriteLine("Le but du jeu est de déplacer les bonbons dans la grille du jeu afin qu'ils se rencontrent et se transforment dans le treat supérieur! Après chaque coup joué, un nouveau bonbon est introduit dans la grille. Le jeu s'arrête une fois que vous aviez atteint le nombre de coups maximale ou qu'il y ait un blocage dans la grille. Facile!");
 else
@@ -63,10 +102,11 @@ Console.WriteLine("--------------------------------------------");
 Console.WriteLine("Choissisez le nombre de coups autorisés:");
 int nbCoups = Convert.ToInt32(Console.ReadLine()!);
 Random aleatoire = new Random();
-char[,] matriceDeJeu = SymboleMatrice(Matrice(4));  //on place le premier bonbon dans une case aléatoire
-matriceDeJeu = SymboleMatrice(matriceDeJeu);        //on place le deuxième bonbon dans une case aléatoire
+int[,] matriceDeJeuEntiers = SymboleMatrice2(MatriceEntiers(4));  //on place le premier bonbon dans une case aléatoire
+matriceDeJeuEntiers = SymboleMatrice2(matriceDeJeuEntiers);        //on place le deuxième bonbon dans une case aléatoire
 Console.WriteLine("Voici votre grille de jeu de départ:");
-AfficherMatrice(matriceDeJeu);
+AfficherMatrice(ConversionMatrice(matriceDeJeuEntiers));
+int score = 0;
 for (int i = 0; i < nbCoups; i++)
 {
     Console.WriteLine("Deplacez les bonbons grace aux touches 8 (↑), 4(<--), 2(↓) et 6(→):");
@@ -77,78 +117,22 @@ for (int i = 0; i < nbCoups; i++)
         {
             case 8:
                 //chercher cases ou se trouvent les bonbons et les deplacer en position [0,j]
-                for (int k = 0; k < 4; k++)
-                {
-                    if (matriceDeJeu[3 - j, k] == '*' && matriceDeJeu[3 - j, k + 1] == matriceDeJeu[3 - j, k])
-                    {
-                        matriceDeJeu[3 - j, k] = ' ';
-                        matriceDeJeu[3 - j, k + 1] = ' ';
-                        matriceDeJeu[0, k] = '@';
-                        AfficherMatrice(matriceDeJeu);
-                    }
-                    else if (matriceDeJeu[3 - j, k] == '*')
-                    {
-                        matriceDeJeu[3 - j, k] = ' ';
-                        matriceDeJeu[0, k] = '*';
-                    }
-                }
+                MoveUp(matriceDeJeuEntiers);
                 break;
 
             case 4:
                 //chercher cases ou se trouvent les bonbons et les deplacer en position [i,0]
-                for (int k = 1; k < 5; k++)
-                {
-                    if (matriceDeJeu[j, 4 - k] == '*' && matriceDeJeu[j, 4 - k - 1] == matriceDeJeu[j, 4 - k])
-                    {
-                        matriceDeJeu[j, 4 - k] = ' ';
-                        matriceDeJeu[j, 4 - k - 1] = ' ';
-                        matriceDeJeu[j, 0] = '@';
-
-                    }
-                    else if (matriceDeJeu[j, 3 - k] == '*')
-                    {
-                        matriceDeJeu[j, 3 - k] = ' ';
-                        matriceDeJeu[j, 0] = '*';
-                    }
-                }
+                MoveLeft(matriceDeJeuEntiers);
                 break;
 
             case 2:
                 //chercher cases ou se trouvent les bonbons et les deplacer en position [3,j]
-                for (int k = 0; k < 4; k++)
-                {
-                    if (matriceDeJeu[j, k] == '*' && matriceDeJeu[j, k] == matriceDeJeu[j + 1, k])
-                    {
-                        matriceDeJeu[j, k] = ' ';
-                        matriceDeJeu[j + 1, k] = ' ';
-                        matriceDeJeu[3, k] = '@';
-                        AfficherMatrice(matriceDeJeu);
-                    }
-                    else if (matriceDeJeu[j, k] == '*')
-                    {
-                        matriceDeJeu[j, k] = ' ';
-                        matriceDeJeu[3, k] = '*';
-                    }
-                }
+                MoveDown(matriceDeJeuEntiers);
                 break;
 
             case 6:
                 //chercher cases ou se trouvent les bonbons et les deplacer en position [i,3]
-                for (int k = 0; k < 4; k++)
-                {
-                    if (matriceDeJeu[j, k] == '*' && matriceDeJeu[j, k + 1] == matriceDeJeu[j, k])
-                    {
-                        matriceDeJeu[j, k] = ' ';
-                        matriceDeJeu[j, k + 1] = ' ';
-                        matriceDeJeu[j, 3] = '@';
-                        AfficherMatrice(matriceDeJeu);
-                    }
-                    else if (matriceDeJeu[j, k] == '*')
-                    {
-                        matriceDeJeu[j, k] = ' ';
-                        matriceDeJeu[j, 3] = '*';
-                    }
-                }
+                MoveRight(matriceDeJeuEntiers);
                 break;
 
             default:
@@ -157,8 +141,169 @@ for (int i = 0; i < nbCoups; i++)
     }
     int l = aleatoire.Next(0, 4);
     int c = aleatoire.Next(0, 4);
-    matriceDeJeu[l, c] = '*';
-    AfficherMatrice(matriceDeJeu);
+    if (matriceDeJeuEntiers[l, c] == 0)
+        matriceDeJeuEntiers[l, c] = 2;
+    else if (l < 3 && c < 3 && matriceDeJeuEntiers[l + 1, c + 1] != 0)
+        matriceDeJeuEntiers[l + 1, c + 1] = 2;
+    AfficherMatrice(ConversionMatrice(matriceDeJeuEntiers));
 }
 
 
+
+// programme qui permet de faire le deplacement en haut
+void MoveUp(int[,] tab)
+{
+    for (int c = 0; c < 4; c++)
+    {
+        for (int l = 1; l < 4; l++)
+        {
+            if (tab[l, c] != 0)
+            {
+                int ligne = l;
+                while (ligne > 0 && tab[ligne - 1, c] == 0)
+                {
+                    tab[ligne - 1, c] = tab[ligne, c];
+                    tab[ligne, c] = 0;
+                    ligne--;
+                }
+                if (ligne > 0 && tab[ligne - 1, c] == tab[ligne, c])
+                {
+                    tab[ligne - 1, c] *= 2;
+                    tab[ligne, c] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+//programme qui permet de faire le deplacement vers la gauche
+void MoveLeft(int[,] tab)
+{
+    for (int c = 1; c < 4; c++)
+    {
+        for (int l = 0; l < 4; l++)
+        {
+            if (tab[l, c] != 0)
+            {
+                int colonne = c;
+                while (colonne > 0 && tab[l, colonne - 1] == 0)
+                {
+                    tab[l, colonne - 1] = tab[l, colonne];
+                    tab[l, colonne] = 0;
+                    colonne--;
+                }
+                if (colonne > 0 && tab[l, colonne - 1] == tab[l, colonne])
+                {
+                    tab[l, colonne - 1] *= 2;
+                    tab[l, colonne] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+//programme qui permet le deplacement vers la droite
+void MoveRight(int[,] tab)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        for (int l = 0; l < 4; l++)
+        {
+            if (tab[l, c] != 0)
+            {
+                int colonne = c;
+                while (colonne < 3 && tab[l, colonne] == 0)
+                {
+                    tab[l, colonne + 1] = tab[l, colonne];
+                    tab[l, colonne] = 0;
+                    colonne++;
+                }
+                if (colonne < 3 && tab[l, colonne] == tab[l, colonne + 1])
+                {
+                    tab[l, colonne + 1] *= 2;
+                    tab[l, colonne] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+//programme qui permet le deplacement vers le bas
+void MoveDown(int[,] tab)
+{
+    for (int c = 0; c < 4; c++)
+    {
+        for (int l = 0; l < 3; l++)
+        {
+            if (tab[l, c] != 0)
+            {
+                int ligne = l;
+                while (ligne < 3 && tab[ligne, c] == 0)
+                {
+                    tab[ligne + 1, c] = tab[ligne, c];
+                    tab[ligne, c] = 0;
+                    ligne++;
+                }
+                if (ligne < 3 && tab[ligne, c] == tab[ligne + 1, c])
+                {
+                    tab[ligne + 1, c] *= 2;
+                    tab[ligne, c] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+//programme qui convertit une matrice d'entiers en matrice de caractères
+char[,] ConversionMatrice(int[,] tab)
+{
+    char[,] matriceJeu = new char[tab.GetLength(0), tab.GetLength(1)];
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            switch (tab[i, j])
+            {
+                case 0:
+                    {
+                        matriceJeu[i, j] = ' ';
+                        break;
+                    }
+
+
+                case 2:
+                    {
+                        matriceJeu[i, j] = '*';
+                        break;
+                    }
+
+                case 4:
+                    {
+                        matriceJeu[i, j] = '@';
+                        break;
+                    }
+
+                case 8:
+                    {
+                        matriceJeu[i, j] = 'o';
+                        break;
+                    }
+
+                case 16:
+                    {
+                        matriceJeu[i, j] = 'J';
+                        break;
+                    }
+
+                default:
+                    break;
+
+            }
+        }
+    }
+    return matriceJeu;
+}
